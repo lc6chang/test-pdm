266e69e3dbac769c005d1fcf0756eaca51811077
lc6chang <lc6chang@gmail.com>
2023-06-15 14:28:08 +0800

    Add example package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_namespace_packages
+
+setup(
+    name='example',
+    version='0.0.1',
+    packages=find_namespace_packages(),
+    install_requires=[
+        'numpy==1.23.3',
+    ]
+)
+
